9d1125c6710a624e137fa2dabeafc06a0adad07f
Tharuka Dilshara <tharukadilshara5@gmail.com>
2024-02-13 07:07:58 +0530

    fix typos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import Link from "next/link";
 export default function Home() {
   return (
    <div>
+    <h1 className="flex text-white ">This is Home Page</h1>
+    <button>
+      <Link href="../analiytics">DASHBOARD</Link>
+    </button>
    </div>
   );
 }
